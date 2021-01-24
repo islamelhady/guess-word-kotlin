7eca2d1b7910395fd098f8c43af6ee39f7f0d783
islamelhady <i.elhady44@gmail.com>
2021-01-24 04:38:16 +0200

    initial

--- app/src/main/java/com/elhady/gesswordkotlin/MainActivity.kt
+++ app/src/main/java/com/elhady/gesswordkotlin/MainActivity.kt
@@ -2,7 +2,9 @@ package com.elhady.gesswordkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * Created by islam elhady on 21-Jan-21.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
